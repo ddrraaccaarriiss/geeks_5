051c577d5148f10c90132a1f3589643ee8210276
ddrraaggaarriiss <okay969696@gmail.com>
2023-05-16 23:02:30 +0300

    'hw1'

---
+++ shop_api/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.static import static
+from shop_api import settings
+from django.contrib import admin
+from django.urls import path
+from product import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/categories/', views.category_api_view),
+    path('api/v1/categories/<int:id>/', views.category_detail_api_view),
+    path('api/v1/products/', views.products_api_view),
+    path('api/v1/products/<int:id>/', views.product_detail_api_view),
+    path('api/v1/reviews/', views.reviews_api_view),
+    path('api/v1/reviews/<int:id>/', views.review_detail_api_view),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
